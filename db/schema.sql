40492ac43e5dc7b4011f1a2d0bef28b96149ce3d
Anita Chengalva <anita.chengalva@gmail.com>
2022-06-01 19:49:12 -0700

    started sql db, walker table

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,10 @@
 DROP DATABASE IF EXISTS p2_db;
 CREATE DATABASE p2_db;
+
+USE p2_db;
+
+CREATE TABLE walkers (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    user_name VARCHAR(30),
+    password VARCHAR(30)
+);
