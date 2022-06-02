c64e5adae2de7908b70588564f21869deb8ba35e
Cameron Buss <camnb97@gmail.com>
2022-06-01 21:13:54 -0700

    event model

---
+++ models/Event.js
@@ -0,0 +1,43 @@
+// starting time
+// Ending Time
+// Date
+// location
+// Service
+// UserId
+// eventBody
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Event extends Model {}
+
+Event.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        start_time: {
+            type: DataTypes.DATE, //TIME ?
+            allowNull: false,
+        },
+        end_date: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        date: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        location: {
+            //should the type be from the google api?
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        service:
+
+    },
+);
+
+module.exports = Event
