bda2942715d145e1748658d6f388741cfc52f043
Anita Chengalva <anita.chengalva@gmail.com>
2022-06-01 20:32:11 -0700

    client table created, linking walker with their clients

--- db/schema.sql
+++ db/schema.sql
@@ -3,8 +3,30 @@ CREATE DATABASE p2_db;
 
 USE p2_db;
 
+-- Dog Walkers Table
+-- Stores user, password, and their clients
 CREATE TABLE walkers (
     id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    user_name VARCHAR(30),
-    password VARCHAR(30)
+    user VARCHAR(30),
+    password VARCHAR(30),
+    clients_id INT,
+    FOREIGN KEY (clients_id),
+    REFERENCES clients (id)
+    ON DELETE SET NULL
+);
+
+-- Client Table
+-- Stores client name, address, contact info, and pet name
+-- Work on linking more than one pet
+CREATE TABLE clients (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    address VARCHAR(100),
+    email VARCHAR(50),
+    phone DECIMAL(10),
+    pets_id INT,
+    FOREIGN KEY (pets_id),
+    REFERENCES pets (id)
+    ON DELETE SET NULL
 );
