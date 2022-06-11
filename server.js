3d9972467d95397af0ec4a78b7ae3d715841fa8f
Jared Teraoka <jteraoka13@gmail.com>
2022-06-11 00:12:39 -0700

    added utils back

--- server.js
+++ server.js
@@ -5,9 +5,6 @@ const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
-
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
