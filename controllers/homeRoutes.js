ae1a747d43fa503118e39c0dcfc088202efa846e
Jared Teraoka <jteraoka13@gmail.com>
2022-06-10 22:44:04 -0700

    my adds

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Pets, Clients, User } = require('../models');
+const { Pets, Clients, User, Calendar } = require('../models');
 const withAuth = require('../utils/auth');
 
 //Get All Pets//
@@ -13,30 +13,9 @@ router.get('/', async (req, res) => {
 
     const pets = petData.map((pet) => pet.get({ plain: true }));
     
-    res.render('homepage', { 
+    res.render('login', { 
       pets, 
       logged_in: req.session.logged_in 
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-//Get Pet 
-router.get('/pet/:id', async (req, res) => {
-  try {
-    const petData = await Pets.findByPk(req.params.id, {
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
-    const pet = petData.get({ plain: true });
-    res.render('pet', {
-      ...pet,
-      logged_in: req.session.logged_in
     });
   } catch (err) {
     res.status(500).json(err);
@@ -48,10 +27,10 @@ router.get('/profile', withAuth, async (req, res) => {
   try {
     const userData = await User.findByPk(req.session.user_id, {
       attributes: { exclude: ['username', 'password'] },
-      include: [{ model: Pets }],
+      include: [{ model: Pets, Calendar, Clients }],
     });
     const user = userData.get({ plain: true });
-    res.render('profile', {
+    res.render('menu', {
       ...user,
       logged_in: true
     });
@@ -60,46 +39,6 @@ router.get('/profile', withAuth, async (req, res) => {
   }
 });
 
-//New Client Page//
-router.get('/newcli', withAuth, async (req, res) => {
-  try {
-    res.render('newcli', {
-      ...user,
-      logged_in: true
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-//View Client Page//
-router.get('/viewcli', withAuth, async (req, res) => {
-  try {
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['username', 'password'] },
-      include: [{ model: Pets }],
-    });
-    const user = userData.get({ plain: true });
-    res.render('profile', {
-      ...user,
-      logged_in: true
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-//Calendar//
-router.get('/calendar', (req, res) => {
-  if (req.session.logged_in) {
-    try {
-      const calendarData = await Calendar.findAll({
-        include: [
-          Calendar
-        ],
-      });
-      const calendar = calendarData.map((calendar) => calendar.get({ plain: true }));
-    res.render('calendar');
 
 //Login Redirect//
 router.get('/login', (req, res) => {
